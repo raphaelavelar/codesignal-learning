a26d129e0ab47edb350aecb15704b2e0f221cabf
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2025-03-21 12:28:35 +0000

    Update code formatting

--- Four-Week Coding Interview Prep in Python/Easy Interview Coding Practice in Python/String Manipulation Without Built-in Methods/Finding Vowel Positions in a String.py
+++ Four-Week Coding Interview Prep in Python/Easy Interview Coding Practice in Python/String Manipulation Without Built-in Methods/Finding Vowel Positions in a String.py
@@ -1,8 +1,8 @@
 def solution(s):
     vowels = list()
-    
+
     for index, character in enumerate(s):
         if character.lower() in ["a", "e", "i", "o", "u"]:
             vowels.append(index)
-    
+
     return vowels
